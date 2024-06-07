5c60bd49b1c360c06ac0236b34ab25e251805889
Revant Tripathi <revanttripathi8@gmail.com>
2024-06-07 19:05:00 +0530

    re

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ connectDB();
 
 
 const corsOptions = {
-    origin: 'https://pathbeatassignment.netlify.app/', // Allow only this origin http://localhost:3000
-  };
+    origin: 'http://localhost:3000', // Allow only this origin
+};
 
 app.use(cors(corsOptions));
 app.use("/data", require("./routes/dataRoutes"));
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log("Server Started")
 })
